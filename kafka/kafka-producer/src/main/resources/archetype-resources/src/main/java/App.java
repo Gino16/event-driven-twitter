af47a9a3ee0ede6b4c6a2a0f902b2838f9285ffb
Gino Ascencio <gino.francisco@hotmail.com>
2023-03-14 08:25:11 -0500

    feat: added kafka configs

---
+++ kafka/kafka-producer/src/main/resources/archetype-resources/src/main/java/App.java
@@ -0,0 +1,13 @@
+package $com.event-driven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
