d4b5bdf2e4c724affb5e9c5ac3c00c1f379c4cfa
gino.francisco@hotmail.com <gino.francisco@hotmail.com>
2023-04-01 15:19:30 -0500

    added encryption JCE method

---
+++ config-server/src/main/java/com/gino/microservices/configserver/config/SecurityConfig.java
@@ -0,0 +1,16 @@
+package com.gino.microservices.configserver.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring()
+                .antMatchers("/encrypt")
+                .antMatchers("/decrypt");
+        super.configure(web);
+    }
+}
