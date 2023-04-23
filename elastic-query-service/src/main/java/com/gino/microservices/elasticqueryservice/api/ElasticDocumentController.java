1f79e41b85a0cc9ba0650a9bf8ef0829fcd936c5
Gino Ascencio <gino.francisco@hotmail.com>
2023-04-22 21:21:23 -0500

    feat: added config to work with spring security

--- elastic-query-service/src/main/java/com/gino/microservices/elasticqueryservice/api/ElasticDocumentController.java
+++ elastic-query-service/src/main/java/com/gino/microservices/elasticqueryservice/api/ElasticDocumentController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/documents")
+@RequestMapping(value = "/documents")
 @Slf4j
 public class ElasticDocumentController {
 
