7e00ff644e9779a331cd0ac28298ec489cd4a032
Gino Ascencio <gino.francisco@hotmail.com>
2023-04-23 19:42:31 -0500

    feat: added versioning with uri

--- elastic-query-service/src/main/java/com/gino/microservices/elasticqueryservice/api/ElasticDocumentController.java
+++ elastic-query-service/src/main/java/com/gino/microservices/elasticqueryservice/api/ElasticDocumentController.java
@@ -3,6 +3,7 @@ package com.gino.microservices.elasticqueryservice.api;
 import com.gino.microservices.elasticqueryservice.business.ElasticQueryService;
 import com.gino.microservices.elasticqueryservice.model.ElasticQueryServiceRequestModel;
 import com.gino.microservices.elasticqueryservice.model.ElasticQueryServiceResponseModel;
+import com.gino.microservices.elasticqueryservice.model.ElasticQueryServiceResponseModelV2;
 import java.util.List;
 import javax.validation.Valid;
 import javax.validation.constraints.NotEmpty;
@@ -25,20 +26,39 @@ public class ElasticDocumentController {
 
   private final ElasticQueryService elasticQueryService;
 
-  @GetMapping({"/", ""})
+  @GetMapping("/v1")
   public @ResponseBody ResponseEntity<List<ElasticQueryServiceResponseModel>> getAllDocuments() {
     return ResponseEntity.ok(elasticQueryService.getAllDocuments());
   }
 
-  @GetMapping("/{id}")
+  @GetMapping("/v1/{id}")
   public @ResponseBody ResponseEntity<ElasticQueryServiceResponseModel> getDocumentsById(
       @PathVariable @NotEmpty String id) {
     return ResponseEntity.ok(elasticQueryService.getDocumentById(id));
   }
 
-  @PostMapping("/get-document-by-text")
+  @GetMapping("/v2/{id}")
+  public @ResponseBody ResponseEntity<ElasticQueryServiceResponseModelV2> getDocumentsByIdV2(
+      @PathVariable @NotEmpty String id) {
+    return ResponseEntity.ok(getV2Model(elasticQueryService.getDocumentById(id)));
+  }
+
+  @PostMapping("/v1/get-document-by-text")
   public @ResponseBody ResponseEntity<List<ElasticQueryServiceResponseModel>> getDocumentsByText(
       @RequestBody @Valid ElasticQueryServiceRequestModel requestModel) {
     return ResponseEntity.ok(elasticQueryService.getDocumentByText(requestModel.getText()));
+  }
+
+  private ElasticQueryServiceResponseModelV2 getV2Model(
+      ElasticQueryServiceResponseModel responseModel) {
+    ElasticQueryServiceResponseModelV2 responseModelV2 = ElasticQueryServiceResponseModelV2
+        .builder()
+        .id(Long.parseLong(responseModel.getId()))
+        .text(responseModel.getText())
+        .userId(responseModel.getUserId())
+        .createdAt(responseModel.getCreatedAt())
+        .build();
+    responseModelV2.add(responseModel.getLinks());
+    return responseModelV2;
   }
 }
---
+++ elastic-query-service/src/main/java/com/gino/microservices/elasticqueryservice/model/ElasticQueryServiceResponseModelV2.java
@@ -0,0 +1,19 @@
+package com.gino.microservices.elasticqueryservice.model;
+
+import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.hateoas.RepresentationModel;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ElasticQueryServiceResponseModelV2 extends RepresentationModel<ElasticQueryServiceResponseModelV2> {
+  private Long id;
+  private Long userId;
+  private String text;
+  private LocalDateTime createdAt;
+}
